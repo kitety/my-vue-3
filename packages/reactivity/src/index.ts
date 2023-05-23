711faac0cef94acb5663166e2f1eeb31b3fa4fe0
kitety <aizaizuori@foxmail.com>
2023-05-23 23:46:10 +0800

    feat: init

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,4 @@
+import { isObject } from "@vue/shared";
+
+console.log(isObject({ a: 1 }));
+export { isObject };
---
+++ scripts/dev.js
@@ -0,0 +1,29 @@
+const { build } = require('esbuild')
+const { resolve } = require('path')
+
+const target = 'reactivity'
+
+build({
+    // 入口文件
+    entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
+    // 打包后的文件
+    outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
+    // 依赖模块全部打包到一起
+    bundle: true,
+    // 支持调试
+    sourcemap: true,
+    // 打包格式 esm es6模式
+    format: 'esm',
+    // 指定平台 浏览器使用
+    platform: 'browser',
+    // watch
+    watch: {
+        onRebuild(error, result) {
+            //文件变化重新构建
+            console.log('rebuild...')
+        }
+    }
+}).then(() => {
+    console.log('watch...')
+
+})
